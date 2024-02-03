10dfbae3136be9a3b5731c0d38a73ee87eefa0bd
Nathyanqui <132240866+Nathyanqui@users.noreply.github.com>
2024-02-03 01:45:20 -0500

    version 1

---
+++ app.js
@@ -0,0 +1,52 @@
+var usuarioText = document.getElementById('usuarioText');
+var botonEncriptar = document.getElementById('btn-encriptador');
+//var botonDesencriptar = document.getElementById('btn-desencriptador');
+//var botonCopiar = document.getElementById('btn-copiar');
+//var desincriptadoText = document.getElementById('desincriptadoText');
+
+
+function buttonEncriptar(){
+    const textDeUsuario = usuarioText.value.toLowerCase();
+    
+    let textoCifrado = textDeUsuario
+    .replace(/e/gi, 'enter')
+    .replace(/i/gi, 'imes')
+    .replace(/a/gi, 'ai')
+    .replace(/o/gi, 'ober')
+    .replace(/u/gi, 'ufat')
+    
+    desincriptadoText.innerHTML = textoCifrado;
+    document.getElementById("mensaje-inical").style.display = "none";
+    document.getElementById("mensaje-descincriptado").style.display = "block";
+}
+
+function desencriptarTexto(){
+    const textDeUsuario = usuarioText.value.toLowerCase();
+    
+    let textoCifrado = textDeUsuario
+    .replace(/enter/gi, 'e')
+    .replace(/imes/gi, 'i')
+    .replace(/ai/gi, 'a')
+    .replace(/ober/gi, 'o')
+    .replace(/ufat/gi, 'u')
+    
+    desincriptadoText.innerHTML = textoCifrado;
+}
+
+
+function copiarTexto(){
+    var contenido = desincriptadoText;
+    contenido.select();
+    document.execCommand("copy")
+}
+
+
+/*
+function ocultarMensajeFinal(){
+  if( usuarioText == ""){
+        document.getElementById("mensaje-descincriptado").style.display = "none";
+    }else{
+        document.getElementById("mensaje-inical").style.display = "none";
+    }
+}
+ocultarMensajeFinal()*/
